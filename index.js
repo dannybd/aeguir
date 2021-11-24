1048f4910f47d0c8a3d09e923ab2430cbc42d174
dannybd <dannybd@mit.edu>
2021-11-24 19:19:53 +0000

    Move index.js to glob.sync to mirror bot.py

--- index.js
+++ index.js
@@ -1,34 +1,34 @@
-// Require the necessary discord.js classes
-const { Client, Intents } = require('discord.js');
-const { token } = require('./config.json');
+const glob = require('glob');
+const path = require('path');
+
+const {Client, Intents} = require('discord.js');
+const {token} = require('./config.json');
 
 // Create a new client instance
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const client = new Client({
+  intents: [
+    Intents.FLAGS.GUILD_MEMBERS,
+    Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+    Intents.FLAGS.GUILD_MESSAGES,
+    Intents.FLAGS.GUILDS,
+  ],
+});
 
 // When the client is ready, run this code (only once)
 client.once('ready', () => {
   console.log('Ready!');
 });
 
-const commands = require('./commands');
-
-client.on('interactionCreate', async interaction => {
-  if (!interaction.isCommand()) {
-    console.log('Not a command!', interaction);
-    return;
-  }
-  const command = commands[interaction.commandName];
-  if (!command) {
-    console.log('Missing command!', interaction);
-    return;
-  }
+console.log('Loading extensions...');
+glob.sync('./extensions/*.js').forEach(file => {
   try {
-    await command.execute(interaction);
+    console.log(`Loading ${file}`);
+    require(path.resolve(file));
   } catch (error) {
-    console.error(error);
-    await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+    console.error(`Failed to load extension ${file}`, error);
   }
 });
 
 // Login to Discord with your client's token
+console.log('Starting!');
 client.login(token);
