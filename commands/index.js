c3ccc769c9734b23c0627023e5db214610124820
dannybd <dannybd@mit.edu>
2021-11-24 19:02:33 +0000

    ping -> poke

--- commands/index.js
+++ commands/index.js
@@ -1,3 +1,3 @@
 module.exports = {
-  'ping': require('./ping'),
+  'poke': require('./poke'),
 };
--- commands/poke.js
+++ commands/poke.js
@@ -2,9 +2,9 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 
 module.exports = {
   data: new SlashCommandBuilder()
-    .setName('ping')
-    .setDescription('Replies with Pong!'),
+    .setName('poke')
+    .setDescription('Tests command is alive'),
   async execute(interaction) {
-    await interaction.reply('Pong!');
+    await interaction.reply('Hello, I am alive');
   },
 };
